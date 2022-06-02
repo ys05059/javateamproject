557f89a64dd3cb05521865b24fbf898785b65768
Raloooo <optimuslove0223@icloud.com>
2022-06-03 03:18:35 +0900

    20220603 0315
    로그인페이지 초본 구현 완료
    
    가입 폼을 작성하면 user파일에 (사용자아이디).data로 저장
    로그인 또는 신규 가입 시 deserializable 해와서 데이터 비교
    
    //구현할 부분 - user에 아무 파일도 없을 때 오류가 날 수 있음.

--- Health_history/src/Login/login.java
+++ Health_history/src/Login/login.java
@@ -16,11 +16,17 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.awt.event.ActionEvent;
 
 import Login.User;
 import Login.login;
-import Login.signuppage;
 import Main.dayRecordpage;
 import set단위class.dayRecord;
 import set단위class.exRecord;
@@ -36,6 +42,9 @@ public class login {
 	/**
 	 * Launch the application.
 	 */
+	
+	ArrayList<User> userinfoList = new ArrayList<User>(); //class 배열리스트 설정
+	
 	public static void main(String[] args){
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -64,8 +73,12 @@ public class login {
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws FileNotFoundException 
 	 */
 	private void initialize() {
+		//회원 정보 불러와 배열리스트에 저장하기.
+		
+
 		frame = new JFrame("Health History");
 		frame.setBounds(100, 100, 700, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
@@ -91,22 +104,42 @@ public class login {
 		loginBtn.setFont(new Font("휴먼모음T", Font.PLAIN, 18));
 		loginBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				String rightid = "shin";
-				String rightpwd = "1234";
-				
-				if(!(rightid.equals(loginField.getText()) && 
-						rightpwd.equals(passwordField.getText()))) {
-					JOptionPane.showMessageDialog(null, "login failed.");
-				}else {
-					JOptionPane.showMessageDialog(null, "login success!!");
-					ArrayList<dayRecord> dR_ary = new ArrayList<>();
-					dayRecordpage frame2 = new dayRecordpage(dR_ary);
-					frame2.setVisible(true);
-					frame.setVisible(false);
-					
-					
+				//회원 가입을 했든 안 했든 유저 정보는 가져와야 한다.
+				userinfoList = bringUserInfo();   //클래스 배열 얕은복사 수행
+				System.out.println(userinfoList.size() + "in login.java");
+				ArrayList<String> idnow = new ArrayList<String>();
+				ArrayList<String> pwnow = new ArrayList<String>();
+				for(int i = 0; i < userinfoList.size(); i++) {
+					idnow.add(userinfoList.get(i).getID());
+					pwnow.add(userinfoList.get(i).getPW());
 				}
+
+				
+				for(int i = 0; i < idnow.size(); i++) {
+					if(idnow.get(i).equals(loginField.getText()) &&  pwnow.get(i).equals(getPasswordInfo())) {
+						JOptionPane.showMessageDialog(null, "login success!!");
+						ArrayList<dayRecord> dR_ary = new ArrayList<>();
+						dayRecordpage frame2 = new dayRecordpage(dR_ary);
+						frame2.setVisible(true);
+						frame.dispose(); //로그인 창 종료
+						break;
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "login failed.");
+						loginField.setText("");
+						passwordField.setText("");
+					}
+				}
+//				if(!(rightid.equals(loginField.getText()) && 
+//						rightpwd.equals(passwordField.getText()))) {
+//					JOptionPane.showMessageDialog(null, "login failed.");
+//				}else {
+//					JOptionPane.showMessageDialog(null, "login success!!");
+//					ArrayList<dayRecord> dR_ary = new ArrayList<>();
+//					dayRecordpage frame2 = new dayRecordpage(dR_ary);
+//					frame2.setVisible(true);
+//					frame.setVisible(false);
+
 			}
 		});
 		loginBtn.setBackground(new Color(0, 0, 139));
@@ -124,8 +157,7 @@ public class login {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				signup A =new signup();
-				
-				
+
 			}
 			
 		});
@@ -133,5 +165,49 @@ public class login {
 		
 		frame.pack();
 		
+	}
+	
+	
+	public ArrayList<User> getUserinfoList() {
+		return userinfoList;
+	}
+
+	public void setUserinfoList(ArrayList<User> userinfoList) {
+		this.userinfoList = userinfoList;
+	}
+
+	public ArrayList<User> bringUserInfo(){
+		ArrayList<User> userinfoList = new ArrayList<User>(); //class 배열리스트 설정
+		File whatsin = new File("user\\");
+		String[] filenames = whatsin.list();
+		
+		if(filenames != null) {
+			for (int i = 0; i < filenames.length; i++) {
+				User AA = new User();
+				try {
+					ObjectInputStream inputStream =
+							new ObjectInputStream (new FileInputStream("user\\" + filenames[i]));
+					AA = (User)inputStream.readObject(); //클래스 정보 가져오기
+					System.out.println(AA.getGender()); 
+					userinfoList.add(AA); //add to ArrayList<user>
+					inputStream.close();
+				}catch(FileNotFoundException e) {
+					System.err.println("can't find file");
+					System.exit(0);
+				}catch(ClassNotFoundException e) {
+					System.err.println("problem occuered");
+					System.exit(0);		
+				}catch(IOException e) {
+					System.err.println("problem occuered");
+					System.exit(0);
+				}
+			}
+		}
+		return userinfoList;
+	}
+	
+	private String getPasswordInfo() {
+		String str = new String(passwordField.getPassword());
+		return str;
 	}
 }
--- Health_history/src/Login/signup.java
+++ Health_history/src/Login/signup.java
@@ -6,9 +6,20 @@ import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -20,43 +31,34 @@ import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
 import javax.swing.Icon;
 
-public class signup extends JFrame {
+import Login.User;
+import Login.login;
+import javax.swing.DefaultComboBoxModel;
+
+public class signup extends JFrame implements ActionListener{
 
 	private JFrame frame;
 	private JTextField idInputFIeld;
 	private JPasswordField passwordField;
 	private JPasswordField passwordField2;
+	private String gender;
 	private JTextField nicknameFIeld;
-	
-	
-	
-	private JComboBox<String> genderCombo;
-	
-	private final String[] gender = {"남자", "여자"};
 	private JTextField inputweightField;
 	private JTextField heightField;
 	private JTextField muscleField;
 	private JTextField bodyField;
-
-	/**
-	 * Launch the application.
-	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					signup window = new signup();
-//					window.frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
-
-	/**
-	 * Create the application.
-	 */
+	private JButton isIDSame;
+	private JButton isPWSame;
+	private JComboBox genderCombo;
+	
+	private User userinfo;
+	private login log;
+	
+	
+	ArrayList<User> userinfoList_signup = new ArrayList<User>(); //class 배열리스트 설정
+	boolean ispushjungbokBtn = false; //중복버튼 눌렀는지 체크
+	boolean ispushinputcheckBtn = false; //입력확인버튼 눌렀는지 쳌
+	
 	public signup() {
 		initialize();
 	}
@@ -65,6 +67,40 @@ public class signup extends JFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		
+//		before signup, we should bring all user info .
+		File whatsin = new File("user\\");
+		String[] filenames = whatsin.list();
+		
+		if(filenames != null) {
+			for (int i = 0; i < filenames.length; i++) {
+				User AA = new User();
+				try {
+					ObjectInputStream inputStream =
+							new ObjectInputStream (new FileInputStream("user\\" + filenames[i]));
+					AA = (User)inputStream.readObject(); //클래스 정보 가져오기
+					System.out.println(AA.getGender()); 
+					userinfoList_signup.add(AA); //add to ArrayList<user>
+					inputStream.close();
+				}catch(FileNotFoundException e) {
+					System.err.println("can't find file");
+					System.exit(0);
+				}catch(ClassNotFoundException e) {
+					System.err.println("prlblem occuered");
+					System.exit(0);		
+				}catch(IOException e) {
+					System.err.println("problem occuered");
+					System.exit(0);
+				}
+			}
+			
+			System.out.println(userinfoList_signup.size());
+		}
+		
+		
+		
+		
 		setVisible(true);
 		setBounds(100, 100, 900, 700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -93,13 +129,42 @@ public class signup extends JFrame {
 		idInputFIeld = new JTextField();
 		idInputFIeld.setFont(new Font("휴먼엑스포", Font.ITALIC, 16));
 		idInputFIeld.setBounds(179, 82, 191, 35);
+		
 		signupPanel.add(idInputFIeld);
 		idInputFIeld.setColumns(10);
 		
-		JButton isIDSame = new JButton("\uC911\uBCF5\uD655\uC778");
+		isIDSame = new JButton("\uC911\uBCF5\uD655\uC778");
 		isIDSame.setBackground(new Color(135, 206, 235));
 		isIDSame.setFont(new Font("휴먼엑스포", Font.BOLD | Font.ITALIC, 16));
 		isIDSame.setBounds(382, 78, 98, 43);
+		isIDSame.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				boolean avail = true;
+				ArrayList<String> idnow = new ArrayList<String>();
+				for(int i = 0; i < userinfoList_signup.size(); i++) {
+					idnow.add(userinfoList_signup.get(i).getID());
+				}
+				for(int i = 0; i < idnow.size(); i++) {
+					if(idnow.get(i).equals(idInputFIeld.getText())) {
+						JOptionPane.showMessageDialog(null, "이미 존재하는 아이디입니다.");
+						idInputFIeld.setText("");
+						avail = false;
+						ispushjungbokBtn = false;
+						break;
+					}
+				}
+				if(avail == true) {
+					JOptionPane.showMessageDialog(null, "사용 가능한 아이디입니다.");
+					ispushjungbokBtn = true;
+				}
+				
+				
+				
+			}
+			
+		});
 		signupPanel.add(isIDSame);
 		
 		ImageIcon PWicon = new ImageIcon("C:\\github\\javateamproject\\Health_history\\icon\\PW.png");
@@ -125,11 +190,30 @@ public class signup extends JFrame {
 		passwordField2.setBounds(179, 225, 191, 35);
 		signupPanel.add(passwordField2);
 		
-		JButton isPWSame = new JButton("\uC785\uB825\uD655\uC778");
+		isPWSame = new JButton("\uC785\uB825\uD655\uC778");
 		isPWSame.setFont(new Font("휴먼엑스포", Font.BOLD | Font.ITALIC, 16));
 		isPWSame.setBackground(new Color(135, 206, 235));
 		isPWSame.setBounds(382, 221, 98, 43);
+		isPWSame.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(getPasswordInfo().equals(getPasswordInfo2())) {
+					JOptionPane.showMessageDialog(null, "동일한 비밀번호입니다.");
+					ispushinputcheckBtn = true;
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "두 입력이 서로 다릅니다. 다시 입력하세요");
+					ispushinputcheckBtn = false;
+					passwordField.setText("");
+					passwordField2.setText("");
+				}
+				
+			}
+			
+		});
 		signupPanel.add(isPWSame);
+		
 		
 		ImageIcon NNicon = new ImageIcon("C:\\github\\javateamproject\\Health_history\\icon\\nickname.png");
 		JLabel NicknameIcon = new JLabel(NNicon);
@@ -157,23 +241,13 @@ public class signup extends JFrame {
 		genderLabel.setBounds(120, 381, 56, 49);
 		signupPanel.add(genderLabel);
 		
-		JRadioButton maleBtn = new JRadioButton("\uB0A8\uC131");
-		maleBtn.setBackground(new Color(135, 206, 250));
-		maleBtn.setSelected(true);
-		maleBtn.setFont(new Font("휴먼엑스포", Font.PLAIN, 18));
-		maleBtn.setBounds(179, 392, 73, 23);
+		genderCombo = new JComboBox();
+		genderCombo.addItem("남성");
+		genderCombo.addItem("여성");
+		genderCombo.setFont(new Font("휴먼엑스포", Font.BOLD, 16));
+		genderCombo.setBounds(179, 390, 73, 35);
+		signupPanel.add(genderCombo);
 		
-		
-		JRadioButton femaleBtn = new JRadioButton("\uC5EC\uC131");
-		femaleBtn.setBackground(new Color(135, 206, 250));
-		femaleBtn.setFont(new Font("휴먼엑스포", Font.PLAIN, 18));
-		femaleBtn.setBounds(297, 392, 73, 23);
-		signupPanel.add(femaleBtn);
-		
-		ButtonGroup malefemale = new ButtonGroup();
-		malefemale.add(maleBtn);
-		malefemale.add(femaleBtn);
-		signupPanel.add(maleBtn);
 		
 		ImageIcon weightIcon= new ImageIcon("C:\\github\\javateamproject\\Health_history\\icon\\weight.png");
 		JLabel weighticon = new JLabel(weightIcon);
@@ -270,11 +344,48 @@ public class signup extends JFrame {
 		
 		
 		registerBtn.addActionListener(new ActionListener(){
-
+			//모두 기입을 했다면, 가입 처리와 동시에 유저 이름으로 파일을 만들고, 유저의 정보를 byte화해서 저장한다.
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				
+				if(ispushjungbokBtn == true && ispushinputcheckBtn == true) {
+					log = new login();
+					
+					userinfo = new User(idInputFIeld.getText(), getPasswordInfo2(), genderCombo.getSelectedItem().toString(),
+							nicknameFIeld.getText(), 
+							Float.valueOf(inputweightField.getText()), Float.valueOf(heightField.getText()), 
+							Float.valueOf(muscleField.getText()) , Float.valueOf(bodyField.getText()));
+					userinfoList_signup.add(userinfo); //유저 정보 업데이트  = signup의 user class arraylist 에 add
+
+					try {
+
+						ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream
+								("user\\" + idInputFIeld.getText() +".dat"));
+						outputStream.writeObject(userinfo);
+						outputStream.close();
+					}catch(IOException e1) {
+						System.err.println("error occuered when writing to file");
+						System.exit(0);
+					}
+					System.out.println(userinfoList_signup);
+					System.out.println(userinfoList_signup.size());
+					
+					log.setUserinfoList(userinfoList_signup);
+					ispushjungbokBtn = false;
+					ispushinputcheckBtn = false;
+							
+					dispose();
+					
+
+				}
+				else if(ispushjungbokBtn == false && ispushinputcheckBtn == false) {
+					JOptionPane.showMessageDialog(null, "중복확인버튼과 비번입력확인버튼을 눌러야 합니다.");
+				}
+				else if(ispushjungbokBtn == true && ispushinputcheckBtn == false) {
+					JOptionPane.showMessageDialog(null, "비번입력확인버튼을 눌러야 합니다.");
+				}
+				else if(ispushjungbokBtn == false && ispushinputcheckBtn == true) {
+					JOptionPane.showMessageDialog(null, "중복확인버튼을 눌러야 합니다.");
+				}				
 				
 			}
 			
@@ -282,10 +393,54 @@ public class signup extends JFrame {
 		
 		signupPanel.add(registerBtn);
 		
-		
-		
-		
-		
+
 		getContentPane().setLayout(null);
 	}
+	
+	private String getPasswordInfo() {
+		String str = new String(passwordField.getPassword());
+		return str;
+	}
+	
+	private String getPasswordInfo2() {
+		String str = new String(passwordField2.getPassword());
+		return str;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource().equals("남성")) {
+			gender = "남성";
+		}
+		else{
+			gender = "여성";
+		}
+	}
+	
+	public void makeDir(String username) { //create directory by username
+		
+		File Directory = new File(username);
+		
+		if (!Directory.exists()) {
+			try {
+				Directory.mkdir();
+				
+			}catch (Exception e) {
+				e.getStackTrace();
+			}
+		}else {
+			JOptionPane.showMessageDialog(null, "이미 가입되어 있는 회원정보입니다.");
+		}
+		
+	}
+	
+	public ArrayList<User> getUserinfoList() {
+		return userinfoList_signup;
+	}
+
+	public void setUserinfoList(ArrayList<User> userinfoList) {
+		this.userinfoList_signup.addAll(userinfoList);
+	}
 }
+
