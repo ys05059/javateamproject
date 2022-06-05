dec2f4d64d3ddfaa8ca5201333a3de9ac3c7574d
ys05059 <ys05059@gmail.com>
2022-06-05 22:44:17 +0900

    통계 기능 구현 완료

--- Health_history/src/희석/PeriodStatisticsDemo.java
+++ Health_history/src/희석/PeriodStatisticsDemo.java
@@ -1,29 +1,37 @@
 package 희석;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.knowm.xchart.PieChart;
+import org.knowm.xchart.PieChartBuilder;
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.XYSeries.XYSeriesRenderStyle;
+import org.knowm.xchart.style.Styler.LegendPosition;
+import org.knowm.xchart.style.lines.SeriesLines;
+import org.knowm.xchart.style.markers.SeriesMarkers;
 
 import set단위class.dayRecord;
-import javax.swing.JScrollPane;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.time.LocalDate;
-import java.util.ArrayList;
-
-import javax.swing.JTextPane;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JScrollBar;
 
 public class PeriodStatisticsDemo extends JFrame {
 
@@ -33,6 +41,8 @@ public class PeriodStatisticsDemo extends JFrame {
 	private JScrollPane scrollPane;
 	private JTextArea startDayTextArea;
 	private JTextArea endDayTextArea;
+	
+	private JPanel chartpanel;
 	
 	private String startDatestr;
 	private String endDatestr;
@@ -47,7 +57,7 @@ public class PeriodStatisticsDemo extends JFrame {
 	public PeriodStatisticsDemo(ArrayList<dayRecord> dR_ary, LocalDate startld, LocalDate endld) {
 		setTitle("WeekStatisticsDemo");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setSize(500,400);
+		setSize(1000,550);
 		getContentPane().setLayout(null);
 		curr_dR_ary = dR_ary;
 		startDatestr=startld.toString();
@@ -72,7 +82,7 @@ public class PeriodStatisticsDemo extends JFrame {
 		getContentPane().add(endDayTextArea);
 	
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(23, 69, 428, 258);
+		scrollPane.setBounds(23, 69, 428, 150);
 		getContentPane().add(scrollPane);
 		
 		textArea = new JTextArea();
@@ -83,5 +93,66 @@ public class PeriodStatisticsDemo extends JFrame {
 		statistics = pfunc.makeTextArea();
 		textArea.setText(statistics);
 		setVisible(true);
+		
+		chartpanel = new JPanel();
+		XYChart chart = new XYChartBuilder().xAxisTitle("날짜").yAxisTitle("총 세트수").width(600).height(400).build();
+		
+		int count_max=10;
+		
+		//ArrayList<dayRecord> tmp_drary = new ArrayList<>(dR_ary);
+		
+		Collections.sort(dR_ary);
+		ArrayList<Date> ary_x = new ArrayList<>();
+	    for (dayRecord dr : dR_ary) {
+	    	DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH");
+	    	Date date = new Date();
+			try {
+				date = df.parse(Integer.toString(dr.getToday_date().getYear())+"-"+Integer.toString(dr.getToday_date().getMonthValue()) +"-"+Integer.toString(dr.getToday_date().getDayOfMonth())+"T09");
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+	    	ary_x.add(date);
+	    }
+	    
+	    //Date date = Date.from(dr.getToday_date().atStartOfDay(ZoneId.systemDefault()).toInstant());
+	    int tmp;
+	    ArrayList<Integer> ary_y = new ArrayList<>();
+	    for (dayRecord dr : dR_ary) {
+	    	 tmp= dr.get_total_countset();
+	    	ary_y.add(tmp);
+	    	if(tmp > count_max) {
+	    		count_max = (tmp/10+1)*10;
+	    	}
+	    }
+	    
+		XYSeries series = chart.addSeries("총 세트수" ,ary_x,ary_y);
+		series.setMarker(SeriesMarkers.CIRCLE);
+		chart.getStyler().setDatePattern("YYYY-MM-dd");
+		chart.getStyler().setLegendPosition(LegendPosition.InsideNE);
+		chart.getStyler().setXAxisLabelRotation(45);
+		chart.getStyler().setYAxisMin(0.0);
+		chart.getStyler().setYAxisMax((double)count_max);
+		XChartPanel<XYChart> panel=new XChartPanel<XYChart>(chart);
+		panel.setBounds(23, 230,428, 250);
+		getContentPane().add(panel);
+		
+		PieChart chart2 = new PieChartBuilder().width(800).height(600).title("카테고리별 세트비율").build();
+	    Color[] sliceColors = new Color[] { new Color(224, 68, 14), new Color(230, 105, 62), new Color(236, 143, 110), new Color(243, 180, 159), new Color(246, 199, 182) };
+	    chart2.getStyler().setSeriesColors(sliceColors);
+	    
+	    HashMap<String, int[]> catemap= pfunc.getCatemap();
+	    Iterator<String> iter = catemap.keySet().iterator();
+	    String key="";
+	    while(iter.hasNext()) {
+	    	key  = iter.next();
+	    	chart2.addSeries(key, catemap.get(key)[1]);
+	    }
+	    
+	    XChartPanel<PieChart> pie_panel=new XChartPanel<PieChart>(chart2);
+	    pie_panel.setBounds(500, 230,428, 250);
+		getContentPane().add(pie_panel);
+		
+	    
 	}
+	
 }
